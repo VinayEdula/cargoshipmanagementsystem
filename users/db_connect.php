56b6b2b0dd28b7e1016cdc02d00e612ce3d714d0
VinayEdula <78456544+VinayEdula@users.noreply.github.com>
2021-05-23 01:22:08 +0530

    Update db_connect.php

--- users/db_connect.php
+++ users/db_connect.php
@@ -1,5 +1,3 @@
 <?php 
 
-$conn= new mysqli('den1.mysql5.gear.host','csms9','So087!6w1~er','csms9')or die("Could not connect to mysql".mysqli_error($con));
-
-
+$conn= new mysqli('den1.mysql6.gear.host','csms11','Hx17AlPH!_b7','csms11')or die("Could not connect to mysql".mysqli_error($con));
