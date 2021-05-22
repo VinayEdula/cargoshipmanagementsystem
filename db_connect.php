05503725e383577975fa6b94eb67b0cd941a2a54
VinayEdula <78456544+VinayEdula@users.noreply.github.com>
2021-05-23 01:15:20 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,3 @@
 <?php 
 
-$conn= new mysqli('den1.mysql5.gear.host','csms1','Zp0t-_73SQXB','csms1')or die("Could not connect to mysql".mysqli_error($con));
-
+$conn= new mysqli('den1.mysql5.gear.host','csms9','So087!6w1~er','csms9')or die("Could not connect to mysql".mysqli_error($con));
